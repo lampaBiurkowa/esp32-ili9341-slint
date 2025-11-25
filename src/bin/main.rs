ee66308567c4f9aa4898e14eaf9f8038a3d44a53
piowal <piotr.walendziak@freymedical.eu>
2025-11-26 00:02:01 +0100

    movin

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,10 +6,17 @@
     holding buffers for the duration of a data transfer."
 )]
 
+use core::ops::Range;
+
 use esp_hal::clock::CpuClock;
+use embedded_hal::digital::OutputPin;
+use embedded_graphics_core::pixelcolor::raw::RawU16;
+use embedded_graphics_core::pixelcolor::Rgb565;
 use esp_hal::main;
 use esp_hal::time::{Duration, Instant};
 use esp_hal::uart::Uart;
+use mipidsi::models::ILI9341Rgb565;
+use slint::platform::software_renderer::{LineBufferProvider, MinimalSoftwareWindow, RepaintBufferType, Rgb565Pixel};
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -22,17 +29,69 @@ extern crate alloc;
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
+slint::slint! {
+    export component MainWindow inherits Window {
+        width: 240px;
+        height: 320px;
+
+        Text {
+            text: "Hello Slint!";
+            horizontal-alignment: center;
+            vertical-alignment: center;
+        }
+    }
+}
+
+struct DrawBuf<'a, Display> {
+    display: Display,
+    buffer: &'a mut [Rgb565Pixel],
+}
+
+impl<DI, RST> LineBufferProvider for &mut DrawBuf<'_, mipidsi::Display<DI, ILI9341Rgb565, RST>>
+where
+    DI: mipidsi::interface::Interface<Word = u8>,
+    RST: OutputPin<Error = core::convert::Infallible>,
+{
+    type TargetPixel = Rgb565Pixel;
+
+    fn process_line(
+        &mut self,
+        line: usize,
+        range: Range<usize>,
+        render_fn: impl FnOnce(&mut [Rgb565Pixel]),
+    ) {
+        let buf = &mut self.buffer[range.clone()];
+        render_fn(buf);
+        self.display
+            .set_pixels(
+                range.start as u16,
+                line as u16,
+                range.end as u16,
+                line as u16,
+        buf.iter().map(|x| Rgb565::from(RawU16::new(x.0)))
+            )
+            .unwrap();
+    }
+}
+
 #[main]
 fn main() -> ! {
     // generator version: 1.0.1
-
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 98768);
     let mut uart = Uart::new(peripherals.UART0, esp_hal::uart::Config::default()).unwrap();
-    uart.write("Hello world!\n".as_bytes()).unwrap();
+    uart.write("Hello world!1\n".as_bytes()).unwrap();
+
+    let mut window = MinimalSoftwareWindow::new(RepaintBufferType::ReusedBuffer);
+    uart.write("Hello2\n".as_bytes()).unwrap();
+    window.set_size(slint::PhysicalSize::new(240, 320));
+    uart.write("Hello3\n".as_bytes()).unwrap();
+
     loop {
+        slint::platform::update_timers_and_animations();
+    uart.write("Hello5\n".as_bytes()).unwrap();
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
     }
